42f6f1ff42f6bb7254f7f349170a1c7194a9ebfc
wajeeh20-meet <wajeeh20@meet.mit.edu>
2018-10-24 12:23:54 -0400

    lab3

---
+++ lab3.py
@@ -0,0 +1,37 @@
+
+import turtle
+turtle.speed(10)
+for s in range (5):
+	
+	turtle.forward(500)
+	turtle.left(216)
+turtle.penup()
+
+
+
+
+
+
+
+
+
+turtle.goto(-400,0)
+turtle.pendown()
+turtle.begin_fill()
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(30)
+turtle.forward(200)
+turtle.right(120)
+turtle.forward(200)
+turtle.end_fill()
+
+
+turtle.penup()
+turtle.addshape("maya.gif")
+turtle.shape("maya.gif")
+turtle.goto(0,0)
+
+
+turtle.mainloop()
